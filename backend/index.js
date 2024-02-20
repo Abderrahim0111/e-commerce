af6583e2558712d82e449fea355700765b02585b
Abderrahim0111 <abderrahi.defaoui@gmail.com>
2024-02-20 18:17:09 +0100

    final

--- backend/index.js
+++ backend/index.js
@@ -3,41 +3,29 @@ const app = express();
 app.use(express.json());
 
 const cors = require("cors");
+app.use(cors());
+
 const env = require("dotenv");
 env.config();
 
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      console.log(`http://localhost:${port}`);
     });
   })
   .catch((err) => {
-    console.error("Connection error", err);
+    console.log(err);
   });
 
-// CORS configuration
-const allowedOrigins = ['http://localhost:5173']; // Add other allowed origins as needed
-const corsOptions = {
-  origin: function(origin, callback) {
-    // Check if the origin is in the allowed origins list or if it's undefined (local request)
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
-
+ 
 const userRouter = require('./routes/userRoutes')
 app.use('/api', userRouter)
 const productRouter = require('./routes/productRoutes')
@@ -46,3 +34,5 @@ const reviewRouter = require('./routes/reviewRoutes')
 app.use('/api', reviewRouter)
 const orderRouter = require('./routes/orderRoutes')
 app.use('/api', orderRouter)
+
+// final
