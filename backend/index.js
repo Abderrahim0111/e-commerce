80cc3a42bae3ca10fb82715a4688047e12539f56
Abderrahim0111 <abderrahi.defaoui@gmail.com>
2024-02-20 18:12:44 +0100

    final

--- backend/index.js
+++ backend/index.js
@@ -3,29 +3,41 @@ const app = express();
 app.use(express.json());
 
 const cors = require("cors");
-app.use(cors());
-
 const env = require("dotenv");
 env.config();
 
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(port, () => {
-      console.log(`http://localhost:${port}`);
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Connection error", err);
   });
 
- 
+// CORS configuration
+const allowedOrigins = ['http://localhost:5173']; // Add other allowed origins as needed
+const corsOptions = {
+  origin: function(origin, callback) {
+    // Check if the origin is in the allowed origins list or if it's undefined (local request)
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
 const userRouter = require('./routes/userRoutes')
 app.use('/api', userRouter)
 const productRouter = require('./routes/productRoutes')
@@ -34,5 +46,3 @@ const reviewRouter = require('./routes/reviewRoutes')
 app.use('/api', reviewRouter)
 const orderRouter = require('./routes/orderRoutes')
 app.use('/api', orderRouter)
-
-// final
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd(), "");
   return {
     server: {
